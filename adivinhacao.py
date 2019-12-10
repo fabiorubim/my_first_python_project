c48a5a89909c3eac9605658342e1ad50748d39c2
Fabio Rubim <frubim@embarcadero.com.br>
2019-12-10 16:29:04 -0300

    Use of break and continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1,total_de_tentativas + 1): #A tentativa é 3, logo adicionar 1 aqui facilita o entendimento
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     #Variáveis para facilitar a legibilidade
     acertou = chute == numero_secreto
@@ -18,6 +22,7 @@ for rodada in range(1,total_de_tentativas + 1): #A tentativa é 3, logo adiciona
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto!")
