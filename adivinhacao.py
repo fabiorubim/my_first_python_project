d1c88b9b9983ac2235d00f7265c96f843a040ac3
esoft13 - Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2019-12-11 17:14:25 -0300

    Inserted difficulty level in game

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,9 +3,20 @@ import random
 print("*********************************")
 print("Bem vindo no jogo de adivinhação!")
 print("*********************************")
-
 numero_secreto = random.randrange(1, 101) #round(random.random() * 100) #random() gera entre 0.0 e 1.0
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil  (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1,total_de_tentativas + 1): #A tentativa é 3, logo adicionar 1 aqui facilita o entendimento
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
