97a96ea8b6be35ab4a38e7c952011d336e52a429
Fabio Rubim <frubim@embarcadero.com.br>
2019-12-18 10:11:18 -0300

    Changed the title

--- jogos.py
+++ jogos.py
@@ -3,7 +3,7 @@ import adivinhacao
 
 def escolhe_jogo():
     print("*********************************")
-    print("*******Escolha o seu jogo!*******")
+    print("****###Escolha o seu jogo!###****")
     print("*********************************")
 
     print("(1) Forca (2) Adivinhação")
