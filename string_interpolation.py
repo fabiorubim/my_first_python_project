f290b1a32df351a131443350463f33c2aaf680fe
Fabio Rubim <fabiorubim@hotmail.com>
2019-12-18 10:36:12 -0300

    Nothing important

--- string_interpolation.py
+++ string_interpolation.py
@@ -16,4 +16,4 @@ print("R${:07.2f}".format(4.5)) #7 caracteres no total, sendo 2 casas decimais  
 
 print("R${:08.3f}".format(4.5)) #7 caracteres no total, sendo 2 casas decimais  -adiciona zeros antes
 
-print("R${:07d}".format(4)) #é possível utilizar com inteiros
+print("R${:07d}".format(4)) #É possível utilizar com inteiros
