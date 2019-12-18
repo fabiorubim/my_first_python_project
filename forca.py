cf0c59fd9941b1b510a0f03b9e3ed776082363e4
esoft13 - Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2019-12-17 23:46:08 -0300

    The development of Forca game was started

--- forca.py
+++ forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
     print("Fim do jogo!")
 
 if (__name__ == "__main__"):
